1cee0ff85d8480c4fb66d4ab844a931dfac7075f
BlokkGaming <108612240+BlokkGaming@users.noreply.github.com>
2022-09-10 13:48:53 +0300

    homework3

---
+++ homework3.py
@@ -0,0 +1,19 @@
+print(''' 
+ 2 | 1
+-------
+ 3 | 4
+''')
+
+x = int(input('x: '))
+y = int(input('y: '))
+
+if x > 0:
+    if y > 0:
+        print('1')
+    else:
+        print('4')
+else:
+    if y > 0:
+        print('2')
+    else:
+        print('3')
