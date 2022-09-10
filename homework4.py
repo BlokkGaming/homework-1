a845517980229cbe04c27f4a1b3dbd997f941684
BlokkGaming <108612240+BlokkGaming@users.noreply.github.com>
2022-09-10 13:49:47 +0300

    homework4

---
+++ homework4.py
@@ -0,0 +1,9 @@
+pl = int(input('номер плоскости: '))
+if pl == 1:
+    print('x = (0; ∞)\ny = (0; ∞)')
+elif pl == 2:
+    print('x = (-∞; 0)\ny = (0; ∞)')
+elif pl == 3:
+    print('x = (-∞; 0)\ny = (-∞; 0)')
+elif pl == 3:
+    print('x = (0; ∞)\ny = (-∞; 0)')
